6db800c3102d46296f065433e6f07bd78f55dea4
evilquinn <evilquinn@gmail.com>
2015-06-27 19:00:39 +0100

    31 done

--- pe31.cpp
+++ pe31.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <cmath>
 #include <set>
+#include <boost/foreach.hpp>
+
+int count_possibilities(int* coins, int num_coins, int target);
 
 std::string& pe31::name()
 {
@@ -31,7 +34,30 @@ void pe31::run()
      * How many different ways can £2 be made using any number of coins?
      *
      */
+    //int coins[] = { 1, 2, 5, 10, 20, 50, 100, 200 };
+    int coins[] = { 200, 100, 50, 20, 10, 5, 2, 1 };
+    const int num_coins = sizeof(coins)/sizeof(int);
+    std::cout << "PE31: " << count_possibilities(coins, num_coins, 200)
+              << std::endl;
+}
 
-    int result = 0;
-    std::cout << "PE31: " << result << std::endl;
+int count_possibilities(int* coins, int num_coins, int target)
+{
+    // base case: no coins in purse, no possibilities
+    if(num_coins < 1) { return 0; };
+    // can we make the target by adding up our single coin?
+    if(num_coins == 1) { return target%num_coins == 0 ? 1 : 0; }
+
+    // count amount of times
+    // possibilities(coins+1, num_coins-1, target-n*coins[0])
+    // where n*coins[0] < target
+    int total = 0;
+    int curr_try = 0;
+    while(curr_try<=target)
+    {
+        total += count_possibilities(&coins[1], num_coins-1, target-curr_try);
+        curr_try += coins[0];
+    }
+
+    return total;
 }
