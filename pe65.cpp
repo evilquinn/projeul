8ad504c3154306c336ad41aa8ee33d83268fc2d2
evilquinn <evilquinn@gmail.com>
2016-10-27 21:12:36 +0100

    algorithm works for continued V2, still to do for e

--- pe65.cpp
+++ pe65.cpp
@@ -8,6 +8,7 @@
 #include "pe65.hpp"
 #include <iostream>
 #include <cmath>
+#include <gint.hpp>
 
 std::string& pe65::name()
 {
@@ -30,6 +31,19 @@ void pe65::run()
     {
         std::cout << ( three_cycle == 1 ? 2 * cycle_n++ : 1 ) << ", ";
         three_cycle = three_cycle == 2 ? 0 : three_cycle + 1;
+    }
+
+    std::cout << std::endl;
+
+    const size_t DIGIT = 2;
+    gint old_denom = 1;
+    gint denom = DIGIT;
+    for ( size_t i = 0; i < 100; ++i )
+    {
+        std::cout << denom << ", ";
+        gint next_denom = ( denom * DIGIT ) + old_denom;
+        old_denom = denom;
+        denom = next_denom;
     }
 
 }
