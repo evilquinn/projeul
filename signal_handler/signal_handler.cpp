dd6aeb61432c6a98d0331ace27b77d0e3416309c
evilquinn <evilquinn@gmail.com>
2018-04-02 04:02:59 +0100

    trim down poor signal_handler output

--- signal_handler/signal_handler.cpp
+++ signal_handler/signal_handler.cpp
@@ -11,30 +11,10 @@ void signal_handler( int sig_num, siginfo_t* sig_info, void* /*data*/ )
     if ( sig_num != 0 )
     {
         std::cout << "signal   : " << sig_num << std::endl;
+        ::psignal( sig_num, "psignal  " );
     }
     if ( sig_info != nullptr )
     {
-        std::cout << "si_signo : " << sig_info->si_signo << "\n"
-                  << "si_errno : " << sig_info->si_errno << "\n"
-                  << "si_code  : " << sig_info->si_code << "\n";
-        switch ( sig_num )
-        {
-        case SIGILL:   // fall through
-        case SIGFPE:   // fall through
-        case SIGSEGV:  // fall through
-        case SIGBUS:   // fall through
-        case SIGTRAP:  // fall through
-        {
-            std::cout << "si_addr  : " << sig_info->si_addr << "\n";
-            break;
-        }
-        default:
-        {
-            // no op
-        }  // end case
-        }  // end switch
-
-        ::psignal( sig_num, "psignal  " );
         ::psiginfo( sig_info, "psiginfo " );
     }
 
