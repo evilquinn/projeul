62935dffa6f4d5f383175869a223e19553a732d2
evilquinn <evilquinn@gmail.com>
2018-09-23 15:25:08 +0100

    fix how we initialise EVP_CIPHER_CTX using new interface as per openssl 1.1.0

--- utils/crypto.cpp
+++ utils/crypto.cpp
@@ -11,11 +11,10 @@ bool enc_encrypt_cipher( bint&             key,
                          bint&             output,
                          const EVP_CIPHER* cipher )
 {
-    EVP_CIPHER_CTX encryption_context;
-    EVP_CIPHER_CTX_init( &encryption_context );
+    EVP_CIPHER_CTX* encryption_context = EVP_CIPHER_CTX_new();
 
     int success = 1;
-    success &= EVP_EncryptInit_ex( &encryption_context,
+    success &= EVP_EncryptInit_ex( encryption_context,
                                    cipher,
                                    nullptr,
                                    (unsigned char*)key,
@@ -30,20 +29,20 @@ bool enc_encrypt_cipher( bint&             key,
 
     int bytes_written_op = 0;
     int bytes_written    = 0;
-    success &= EVP_EncryptUpdate( &encryption_context,
+    success &= EVP_EncryptUpdate( encryption_context,
                                   (unsigned char*)output,
                                   &bytes_written_op,
                                   (const unsigned char*)input,
                                   input.size() );
     bytes_written += bytes_written_op;
 
-    success &= EVP_EncryptFinal_ex( &encryption_context,
+    success &= EVP_EncryptFinal_ex( encryption_context,
                                     (unsigned char*)output + bytes_written,
                                     &bytes_written_op );
     bytes_written += bytes_written_op;
     output.resize_chararray( bytes_written );
 
-    EVP_CIPHER_CTX_cleanup( &encryption_context );
+    EVP_CIPHER_CTX_free( encryption_context );
 
     return success != 0;
 }
@@ -54,16 +53,15 @@ bool enc_decrypt_cipher( bint&             key,
                          bint&             output,
                          const EVP_CIPHER* cipher )
 {
-    EVP_CIPHER_CTX encryption_context;
-    EVP_CIPHER_CTX_init( &encryption_context );
+    EVP_CIPHER_CTX* encryption_context = EVP_CIPHER_CTX_new();
 
     int success = 1;
-    success &= EVP_DecryptInit_ex( &encryption_context,
+    success &= EVP_DecryptInit_ex( encryption_context,
                                    cipher,
                                    nullptr,
                                    (unsigned char*)key,
                                    (unsigned char*)iv );
-    // EVP_CIPHER_CTX_set_padding(&encryption_context, 0);
+    // EVP_CIPHER_CTX_set_padding(encryption_context, 0);
 
     // length of binary ciphertext is AT MOST =
     // plaintext_size + block_size - 1
@@ -73,20 +71,20 @@ bool enc_decrypt_cipher( bint&             key,
 
     int bytes_written_op = 0;
     int bytes_written    = 0;
-    success &= EVP_DecryptUpdate( &encryption_context,
+    success &= EVP_DecryptUpdate( encryption_context,
                                   (unsigned char*)output,
                                   &bytes_written_op,
                                   (const unsigned char*)input,
                                   input.size() );
     bytes_written += bytes_written_op;
 
-    success &= EVP_DecryptFinal_ex( &encryption_context,
+    success &= EVP_DecryptFinal_ex( encryption_context,
                                     (unsigned char*)output + bytes_written,
                                     &bytes_written_op );
     bytes_written += bytes_written_op;
     output.resize_chararray( bytes_written );
 
-    EVP_CIPHER_CTX_cleanup( &encryption_context );
+    EVP_CIPHER_CTX_free( encryption_context );
 
     return success != 0;
 }
