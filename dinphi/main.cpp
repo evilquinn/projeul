f6a3e281581d81ae181ca0153b9108497b11019a
evilquinn <evilquinn@gmail.com>
2019-01-19 23:04:31 +0000

    format changes for dinphi

--- dinphi/main.cpp
+++ dinphi/main.cpp
@@ -1,48 +1,43 @@
 
 
+#include <stdio.h>
+#include <chrono>
 #include <iostream>
 #include <mutex>
+#include <signal_handler.hpp>
 #include <string>
 #include <thread>
-#include <chrono>
 #include <vector>
-#include <stdio.h>
-#include <signal_handler.hpp>
 
-static const auto dine_duration = std::chrono::milliseconds(2);
-static const auto think_duration = std::chrono::milliseconds(2);
+static const auto dine_duration  = std::chrono::milliseconds( 2 );
+static const auto think_duration = std::chrono::milliseconds( 2 );
 
-static bool stop = false;
+static bool stop                        = false;
 static const int number_of_philosophers = 5;
-static std::vector<std::timed_mutex> table(number_of_philosophers);
-static std::vector<size_t> counts(number_of_philosophers);
+static std::vector<std::timed_mutex> table( number_of_philosophers );
+static std::vector<size_t> counts( number_of_philosophers );
 
 class phi
 {
 public:
-    phi(size_t id) :
-        id_(id)
-    {}
+    phi( size_t id ) : id_( id ) {}
 
     void eat()
     {
-        const size_t left_index = id_;
-        const size_t right_index = id_ == table.size()-1 ? 0 : id_+1;
+        const size_t left_index  = id_;
+        const size_t right_index = id_ == table.size() - 1 ? 0 : id_ + 1;
 
-        std::lock(table[left_index], table[right_index]);
+        std::lock( table[left_index], table[right_index] );
 
-        std::unique_lock<std::timed_mutex> left(table[left_index],
-                                                std::adopt_lock);
-        std::unique_lock<std::timed_mutex> right(table[right_index],
-                                                 std::adopt_lock);
+        std::unique_lock<std::timed_mutex> left( table[left_index],
+                                                 std::adopt_lock );
+        std::unique_lock<std::timed_mutex> right( table[right_index],
+                                                  std::adopt_lock );
 
-        std::this_thread::sleep_for(dine_duration);
+        std::this_thread::sleep_for( dine_duration );
     }
 
-    void think()
-    {
-        std::this_thread::sleep_for(think_duration);
-    }
+    void think() { std::this_thread::sleep_for( think_duration ); }
 
     void dine()
     {
@@ -64,16 +59,16 @@ int main()
     std::vector<phi> phis;
     for ( size_t id = 0; id < number_of_philosophers; ++id )
     {
-        phis.emplace_back(id);
+        phis.emplace_back( id );
     }
     std::vector<std::thread> threads;
     for ( const auto& p : phis )
     {
-        threads.emplace_back(&phi::dine, p);
+        threads.emplace_back( &phi::dine, p );
     }
 
-    const auto run_duration = std::chrono::seconds(10);
-    std::this_thread::sleep_for(run_duration);
+    const auto run_duration = std::chrono::seconds( 10 );
+    std::this_thread::sleep_for( run_duration );
     stop = true;
 
     for ( auto& t : threads )
@@ -83,8 +78,7 @@ int main()
 
     for ( size_t i = 0; i < counts.size(); ++i )
     {
-        std::cout << "phi " << i
-                  << " ate and thought " << counts[i]
+        std::cout << "phi " << i << " ate and thought " << counts[i]
                   << " times." << std::endl;
     }
 
