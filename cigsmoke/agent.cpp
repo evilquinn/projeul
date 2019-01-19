2f9fb5dfb659cdb1e79ac265152ae12b4c846ea1
evilquinn <evilquinn@gmail.com>
2019-01-19 23:16:12 +0000

    ensure agent can't place two of the same items

--- cigsmoke/agent.cpp
+++ cigsmoke/agent.cpp
@@ -15,8 +15,13 @@ void evilquinn::cig_smokers::agent::replace_materials(
     static boost::random::mt19937 rng(
         static_cast<unsigned int>( std::time( nullptr ) ) );
     static boost::random::uniform_int_distribution<> dist( 0, 2 );
+    material m0 = static_cast<material>( dist( rng ) );
+    material m1 = static_cast<material>( dist( rng ) );
+    while ( m1 == m0 )
+    {
+        m1 = static_cast<material>( dist( rng ) );
+    }
+
     boost::asio::post( *asio_,
-                       std::bind( std::move( provide_materials ),
-                                  static_cast<material>( dist( rng ) ),
-                                  static_cast<material>( dist( rng ) ) ) );
+                       std::bind( std::move( provide_materials ), m0, m1 ) );
 }
