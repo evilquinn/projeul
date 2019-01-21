a32a493f5152602b290a2f396f51692fe2a99c62
evilquinn <evilquinn@gmail.com>
2019-01-21 23:25:08 +0000

    read just a few bytes of the line to detect if it's a new log message or continuation as opposed to always reading the full line to do the same check

--- log_file_stream/log_file_stream.cpp
+++ log_file_stream/log_file_stream.cpp
@@ -15,18 +15,42 @@ template<typename IStream>
 IStream& getlogline(IStream& log_stream, std::string& log_line)
 {
     static const std::regex log_line_begin_matcher("^<\\d\\d\\d>\\d \\d\\d\\d\\d");
+    static const size_t log_line_begin_chars = 11;
     std::ostringstream line_acca;
+
+    /*
+     * continually read just enough of each line to determine if the data
+     * just read is the beginning of a new log line, or the continuation of
+     * a multiline log message.
+     * If we got any data, and it's _not_ the beginning of the _next_ log
+     * message, reset the stream position and grab the whole line.
+     * Rinse, repeat, until we detect the next log line, reset the position
+     * on the stream and bail out.
+     **/
     bool first_line = true;
     auto pos_cache = log_stream.tellg();
-    for ( std::string line; std::getline(log_stream, line); )
+    for ( char log_line_begin_buf[log_line_begin_chars+1] = { '\0' };
+          log_stream.read(log_line_begin_buf, log_line_begin_chars) ||
+          log_stream.gcount() > 0; )
     {
-        if ( !first_line && std::regex_search(line, log_line_begin_matcher) )
+        // make sure we're null terminated
+        log_line_begin_buf[log_stream.gcount()] = '\0';
+        if ( !first_line && std::regex_search(log_line_begin_buf,
+                                              log_line_begin_matcher) )
         {
             // reset the stream to effectively put the line back in
             // for next time
             log_stream.seekg(pos_cache);
             break;
         }
+
+        // put the log_line_begin_buf stuff back and grab the whole line.
+        // clear down any failed state which we may have encountered by the
+        // read for the log_line_begin_buf match earlier
+        log_stream.clear();
+        log_stream.seekg(pos_cache);
+        std::string line;
+        std::getline(log_stream, line);
         // remember this position to revert to if we detect the next line is
         // a new log beginning
         pos_cache = log_stream.tellg();
@@ -37,6 +61,7 @@ IStream& getlogline(IStream& log_stream, std::string& log_line)
         line_acca << line;
         first_line = false;
     }
+    // return whatever we got
     log_line = std::move(line_acca.str());
     if ( !log_line.empty() )
     {
