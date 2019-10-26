97e5f415e18e24d59e1bf07848fd77d0ec12480e
evilquinn <evilquinn@gmail.com>
2019-10-26 02:04:15 +0100

    use variant to consume either_or

--- spirit/main.cpp
+++ spirit/main.cpp
@@ -2,6 +2,7 @@
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <string>
 #include <boost/optional.hpp>
+#include <boost/variant.hpp>
 #include <iostream>
 #include <vector>
 
@@ -10,6 +11,9 @@ struct c_text_data
     char colour;
     boost::optional<std::string> opt_text;
 };
+
+typedef boost::optional<std::string> optional_text;
+typedef boost::variant<optional_text, c_text_data> opt_or_c_text;
 
 std::ostream& operator<< (std::ostream& os, const struct c_text_data& c)
 {
@@ -21,25 +25,53 @@ std::ostream& operator<< (std::ostream& os, const struct c_text_data& c)
     return os;
 }
 
+std::ostream& operator<< (std::ostream& os, const optional_text& opt)
+{
+    if ( opt )
+    {
+        os << ", text: \"" << *opt << "\"";
+    }
+    return os;
+}
+
+class opt_or_c_printer : public boost::static_visitor<>
+{
+public:
+    opt_or_c_printer(std::ostream& os) : os_(os) {}
+    template <typename T>
+    void operator()(const T& t) const
+    {
+        os_ << t;
+    }
+private:
+    std::ostream& os_;
+};
+
+std::ostream& operator<<(std::ostream& os, const opt_or_c_text& g)
+{
+    opt_or_c_printer p(os);
+    boost::apply_visitor(p, g);
+    return os;
+}
+
 BOOST_FUSION_ADAPT_STRUCT(
     c_text_data,
     (char, colour)
     (boost::optional<std::string>, opt_text)
 )
 
-typedef boost::optional<std::string> optional_text;
-typedef boost::variant<optional_text, c_text_data> opt_or_c_text;
 
 template <typename Iterator>
 struct sku_grammar : boost::spirit::qi::grammar<Iterator,
-                                                c_text_data()>
+                                                opt_or_c_text()>
 {
-    sku_grammar() : sku_grammar::base_type{c_text}
+    sku_grammar() : sku_grammar::base_type{either_or}
     {
         text %= +( ( boost::spirit::ascii::char_ - ']') - ')' );
         opt_text %= -text;
         c_text %= '_' >> boost::spirit::ascii::char_ >> '(' >> opt_text >> ')';
         ra_text %= '[' >> opt_text >> ']';
+        either_or %= c_text | ra_text;
 
     }
 
@@ -47,7 +79,7 @@ struct sku_grammar : boost::spirit::qi::grammar<Iterator,
     boost::spirit::qi::rule<Iterator, boost::optional<std::string>()> opt_text;
     boost::spirit::qi::rule<Iterator, c_text_data()> c_text;
     boost::spirit::qi::rule<Iterator, boost::optional<std::string>()> ra_text;
-    //boost::spirit::qi::rule<Iterator, boost::optional<std::string>()> ra_text;
+    boost::spirit::qi::rule<Iterator, opt_or_c_text()> either_or;
 };
 
 int main()
@@ -69,14 +101,10 @@ int main()
         sku_grammar<std::string::iterator> h;
         boost::optional<std::string> result;
         c_text_data c_result;
-        if (boost::spirit::qi::parse(it, s.end(), h, c_result))
+        opt_or_c_text o_result;
+        if (boost::spirit::qi::parse(it, s.end(), h, o_result))
         {
-            std::cout << "matched: " << c_result;
-//            std::cout << "matched";
-//            if ( result )
-//            {
-//                std::cout << ": text: \"" << *result << "\"";
-//            }
+            std::cout << "matched: " << o_result;
             if ( it != s.end() )
             {
                 std::cout << ", remaining: \""
