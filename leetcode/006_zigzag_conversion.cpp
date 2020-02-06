d3061326896d1e88e69fcd7257cf24a7af97bb20
evilquinn <evilquinn@gmail.com>
2020-02-06 00:34:12 +0000

    leetcode 006 nailed first go

--- leetcode/006_zigzag_conversion.cpp
+++ leetcode/006_zigzag_conversion.cpp
@@ -35,7 +35,28 @@ class solution {
 public:
     static std::string convert(std::string s, int numRows)
     {
-        return s.substr(0, numRows);
+        auto r = std::string();
+        int bound = ( numRows - 1 ) * 2;
+        if ( bound == 0 ) return s;
+        for ( int i = 0; i < numRows; ++i )
+        {
+            // i represents the index to begin each line with
+            // step represents the gap to the next char
+            std::pair<int, int> step = { bound - ( i * 2 ), i * 2 };
+            if ( step.first <= 0 )
+            {
+                step.first = step.second;
+            }
+            else if ( step.second == 0 )
+            {
+                step.second = step.first;
+            }
+            bool first = true;
+            for ( int j = i;
+                  j < (int)s.size();
+                  r.push_back(s[j]), j += ( first ? step.first : step.second ), first = !(first));
+        }
+        return r;
     }
 };
 
@@ -43,11 +64,13 @@ int main()
 {
     std::vector<std::pair<std::string, int> > data = {
         { "PAYPALISHIRING", 3 },
-        { "PAYPALISHIRING", 4 }
+        { "PAYPALISHIRING", 4 },
+        { "A", 1 },
+        { "AB", 1 }
     };
     for ( auto&& datum : data )
     {
-        std::cout << datum.first << ", " << datum.second << "\n";
+        std::cout << datum.first << ", " << datum.second << ": " << solution::convert(datum.first, datum.second) << "\n";
     }
     return 0;
 }
