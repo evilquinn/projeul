1543299ee8d93727cef63b055b4d4fb6d306bb4e
evilquinn <evilquinn@gmail.com>
2020-02-15 03:05:11 +0000

    010 nearly there...

--- leetcode/010_regular_expression_matching.cpp
+++ leetcode/010_regular_expression_matching.cpp
@@ -52,7 +52,6 @@ public:
     static bool is_match(std::string s, std::string p)
     {
         auto its = std::mismatch(s.begin(), s.end(), p.begin(), p.end());
-        auto bt = its.first;
         for ( ;
               its.second != p.end();
               its = std::mismatch(its.first, s.end(), its.second, p.end()) )
@@ -62,37 +61,47 @@ public:
                 // skip a dot
                 if ( its.first == s.end() )
                 {
+                    // no point, fail
                     return false;
                 }
                 ++its.first;
                 ++its.second;
-                continue;
             }
-            if ( *its.second == '*' )
+            else if ( *its.second == '*' )
             {
                 // handle asterisk
                 if ( its.second == p.begin() ) throw std::runtime_error("asterisk has no preceding element");
                 auto match = *std::prev(its.second);
                 ++its.second;
-                for ( ; its.first != s.end() && ( match == '.' || match == *its.first ); ++its.first );
-                bt = std::prev(its.first, std::distance(its.second, p.end()));
-                continue;
+                if ( its.second != p.end() && *its.second == '.' ) ++its.second;
+                for ( ; its.first != s.end() && ( match == '.' || match == *its.first ); ++its.first )
+                {
+                    while ( its.second != p.end() && std::next(its.second) != p.end() && *std::next(its.second) == '*' )
+                    {
+                        // skip a zeroable element
+                        std::advance(its.second, 2);
+                    }
+                    if ( *its.second != '*' && ( *its.first == *its.second || *its.second == '.' ) )
+                    {
+                        ++its.second;
+                    }
+                }
             }
-            if ( its.second != std::prev(p.end()) && *std::next(its.second) == '*' )
+            else if ( its.second != std::prev(p.end()) && *std::next(its.second) == '*' )
             {
                 // skip a zeroable element
                 std::advance(its.second, 2);
-                continue;
             }
-            return false;
+            else
+            {
+                return false;
+            }
         }
         while ( its.second != p.end() && std::next(its.second) != p.end() && *std::next(its.second) == '*' )
         {
             // skip a zeroable element
             std::advance(its.second, 2);
-            continue;
         }
-        // backtrack
 
 
         return its.first == s.end() && its.second == p.end();
@@ -102,6 +111,7 @@ public:
 int main()
 {
     std::vector<std::pair<std::pair<std::string, std::string>, bool> > data = {
+        { { "aaa", "ab*a*c*a" }, true },
         { { "aa", "a" }, false },
         { { "aa", "a*" }, true },
         { { "ab", ".*" }, true },
@@ -110,6 +120,10 @@ int main()
         { { "aaa", "a*a" }, true },
         { { "aaaaa", "a*a" }, true },
         { { "aaaab", ".*ab" }, true },
+        { { "aaa", "ab*a*c*a" }, true },
+        { { "ab", ".*.." }, true },
+        { { "mississippi", "mis*is*ip*." }, true },
+        { { "ab", ".*c" }, false }
     };
     for ( auto&& datum : data )
     {
