ab3a31eeab0ae6855bcadff1e7b26ddfea725b3c
evilquinn <evilquinn@gmail.com>
2020-02-20 21:20:14 +0000

    aoc day 6 part 2 done, but _really_ ugly

--- aoc/06_2.cpp
+++ aoc/06_2.cpp
@@ -57,8 +57,11 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <queue>
+#include <unordered_set>
 #include <unordered_map>
 #include <map>
+#include <algorithm>
 #include <boost/algorithm/string.hpp>
 
 #if !defined(PROJEUL_AOC_PATH)
@@ -82,51 +85,154 @@ const std::string orbit_map =
 
 struct orbitum
 {
-    int count = 0;
+    std::string name;
     orbitum* orbitee;
     std::vector<orbitum*> orbiters;
 };
 
-int sum_counts(orbitum* o)
+struct orbits
 {
-    int result = 0;
-    if ( o->orbitee )
+    std::unordered_map<std::string, struct orbitum> mem_;
+    struct orbitum* com_;
+    orbits(std::istream& ins) :
+        mem_(),
+        com_()
     {
-        o->count = o->orbitee->count + 1;
-        result += o->count;
+        std::string orbit;
+        while ( ins >> orbit )
+        {
+            auto split = orbit.find(')');
+            std::string so = orbit.substr(0, split);
+            orbitum& orbitee = mem_[so];
+            if ( so.compare("COM") == 0 )
+            {
+                com_ = &orbitee;
+                com_->name = "COM";
+            }
+            std::string name = orbit.substr(split+1);
+            orbitum& orbiter = mem_[name];
+            orbiter.name = name;
+            orbiter.orbitee = &orbitee;
+            orbitee.orbiters.push_back(&orbiter);
+        }
+        if ( com_ == nullptr ) throw std::runtime_error("didn't find com! wtf?");
     }
-    for ( auto&& orbiter : o->orbiters )
-    {
-        result += sum_counts(orbiter);
-    }
-    return result;
-}
+};
 
-orbitum* common_ancestor(orbitum* a, orbitum* b)
+class orbits_walker_euler
 {
-    orbitum* result = nullptr;
-    return result;
-}
+public:
+    orbits_walker_euler(orbitum* orb) :
+        curr_(orb),
+        nexts_(),
+        euler_dists_(),
+        level_(0)
+    {
+        traverse_and_map();
+    }
+    ~orbits_walker_euler()
+    {
+        //dump_euler_dists();
+    }
+    void dump_euler_dists()
+    {
+        for ( auto&& dist : euler_dists_ )
+        {
+            std::cout << "{ " << dist.first << ", " << dist.second << "}\n";
+        }
+    }
+    void traverse_and_map()
+    {
+        euler_dists_.push_back({ curr_->name, level_ });
+        while ( advance() != nullptr ) {};
+    }
 
+    orbitum* advance(int n = 1)
+    {
+        for ( ; n > 0 && curr_ != nullptr; --n )
+        {
+            do
+            {
+                if ( try_next_child() )
+                {
+                    break;
+                }
+            } while ( try_parent() );
+        }
+        return curr_;
+    }
+    int dist_between(const std::string& a, const std::string& b)
+    {
+        std::unordered_set<std::string> ss = { a, b };
+        auto ff = std::find_first_of(euler_dists_.begin(), euler_dists_.end(), ss.begin(), ss.end(),
+            [&](const decltype(euler_dists_)::value_type& l, const std::string& r)
+            {
+                return l.first == r;
+            });
+        ss.erase(ff->first);
+        auto fs = std::find_if(std::next(ff), euler_dists_.end(),
+            [&](const decltype(euler_dists_)::value_type& l)
+            {
+                return l.first == *ss.begin();
+            });
+        auto lca = std::min_element(ff, fs,
+            [&](const decltype(euler_dists_)::value_type& l, const decltype(euler_dists_)::value_type& r)
+            {
+                return l.second < r.second;
+            });
+        return ff->second + fs->second - ( 2 * ( 1 + lca->second ) );
+    }
+
+private:
+    bool try_next_child()
+    {
+        // find out idx for the next child to visit for
+        // this node
+        int& next_child_idx = nexts_[curr_];
+        if ( next_child_idx == 0 )
+        {
+            // if we got the first child, increment the depth
+            ++level_;
+        }
+        else
+        {
+            // if we're trying subsequent children, mark coming
+            // through the parent
+            euler_dists_.push_back({ curr_->name, level_ - 1 });
+        }
+
+        // no more children, bail
+        if ( next_child_idx >= (int)(curr_->orbiters.size()) ) return false;
+
+        // get next
+        curr_ = curr_->orbiters[next_child_idx];
+
+        // and mark it in the path
+        euler_dists_.push_back({ curr_->name, level_ });
+
+        // and remember to visit the next child
+        ++next_child_idx;
+        return true;
+    }
+    bool try_parent()
+    {
+        // we're walking back levels
+        --level_;
+        curr_ = curr_->orbitee;
+        return curr_ != nullptr;
+    }
+
+    orbitum* curr_;
+    std::unordered_map<orbitum*, int> nexts_;
+    std::vector<std::pair<std::string, int> > euler_dists_;
+    int level_;
+};
 
 int distance_between(std::istream& ins, const std::string& a, const std::string& b)
 {
-    std::string orbit;
-    std::unordered_map<std::string, struct orbitum> orbits;
-    struct orbitum* com = nullptr, * noda = nullptr, * nodb = nullptr;
-    while ( ins >> orbit )
-    {
-        auto split = orbit.find(')');
-        std::string so = orbit.substr(0, split);
-        orbitum& orbitee = orbits[so];
-        if ( so.compare("COM") == 0 ) com = &orbitee;
-        else if ( so == a ) noda = &orbitee;
-        else if ( so == b ) nodb = &orbitee;
-        orbitum& orbiter = orbits[orbit.substr(split+1)];
-        orbiter.orbitee = &orbitee;
-        orbitee.orbiters.push_back(&orbiter);
-    }
-    return sum_counts(com);
+    struct orbits orbs(ins);
+    orbits_walker_euler eulorb(orbs.com_);
+    return eulorb.dist_between(a, b);
 }
 
 int distance_between(const std::string& s, const std::string& a, const std::string& b)
