6bd2ce315deca3034b4544a411f03be545396ca6
evilquinn <evilquinn@gmail.com>
2020-02-23 14:28:35 +0000

    aoc 08_1 pretty poor start

--- aoc/08_1.cpp
+++ aoc/08_1.cpp
@@ -37,16 +37,91 @@
 
 
 #include <iostream>
+#include <vector>
 #include <fstream>
 
 
 #if !defined(PROJEUL_AOC_PATH)
 #define PROJEUL_AOC_PATH "."
 #endif
+/**
+struct layer
+{
+    typedef std::vector<int> line;
+    std::vector<line> lines;
+}**/
+
+using line = std::string;
+using layer = std::vector<line>;
+using picture = std::vector<layer>;
+
+picture to_picture(const std::string& s, size_t w, size_t h)
+{
+    picture result = {};
+    auto it = s.begin();
+    while ( it != s.end() )
+    {
+        result.push_back({});
+        for ( size_t i = 0; i < h; ++i )
+        {
+            result.rbegin()->push_back({});
+            std::copy(it, std::next(it, w), std::back_inserter(*result.rbegin()->rbegin()));
+            std::advance(it, w);
+        }
+    }
+    return result;
+}
+
+int elf_check(const picture& pic)
+{
+    // find the layer that contains the fewest 0 digits. On that layer, what
+    // is the number of 1 digits multiplied by the number of 2 digits?
+    int num_zeroes = std::numeric_limits<int>::max();
+    auto least_zeroes = pic.end();
+    for ( auto&& layer : pic )
+    {
+        int num_zeroes_this_layer = 0;
+        for ( auto&& line : layer )
+        {
+            num_zeroes_this_layer += std::count(line.begin(), line.end(), 0);
+        }
+        if ( num_zeroes_this_layer < num_zeroes )
+        {
+            num_zeroes = num_zeroes_this_layer;
+            // todo: fix least_zeroes
+        }
+
+    }
+
+}
+
+std::ostream& operator<<(std::ostream& os, const picture& p)
+{
+    std::string space = "";
+    int idx = 0;
+    for ( auto&& layer : p )
+    {
+        os << "Layer " << ++idx << ":\n";
+        for ( auto&& line : layer )
+        {
+            os << space << line << "\n";
+        }
+        space.append("  ");
+    }
+    return os;
+}
 
 int main()
 {
+    std::vector<std::pair<std::string, std::pair<int, int> > > data = {
+        { "123456789012", { 3, 2 } }
+    };
 
+    for ( auto&& datum : data )
+    {
+        std::cout << to_picture(datum.first, datum.second.first, datum.second.second) << "\n";
+    }
+/**
     std::ifstream ifs(PROJEUL_AOC_PATH "/08_input.txt");
     std::string ins;
     while ( ifs >> ins )
@@ -54,7 +129,7 @@ int main()
         std::cout << ins << std::endl;
 
     }
-
+*/
 
     return 0;
 }
