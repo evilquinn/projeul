a1d877c1f2604155b757f6c1516a713b496e24d7
evilquinn <evilquinn@gmail.com>
2020-12-03 00:45:22 +0000

    aoc2020 day 1 part 2 done

--- aoc2020/01_1.cpp
+++ aoc2020/01_1.cpp
@@ -5,6 +5,44 @@
 #include <boost/lexical_cast.hpp>
 #include "../aoc/path_def.hpp"
 
+size_t find_product_for_recurs(const std::vector<size_t>& data, size_t offset, size_t num_cands, size_t sum_target)
+{
+    size_t result = 0;
+    if ( num_cands == 2 )
+    {
+        for ( auto it = std::next(data.begin(), offset); it != data.end(); ++it )
+        {
+            size_t cand = sum_target - *it;
+            if ( std::binary_search(std::next(it), data.end(), cand) )
+            {
+                result = *it * cand;
+                std::cout << "cands: " << num_cands << ", target: " << sum_target << ", answer: " << result << std::endl;
+                break;
+            }
+        }
+    }
+    else if ( num_cands > 2 )
+    {
+        for ( size_t i = offset; i < data.size(); ++i )
+        {
+            size_t next_target = sum_target - data[i];
+            size_t cand_result = find_product_for_recurs(data, i + 1, num_cands - 1, next_target);
+            if ( cand_result != 0 )
+            {
+                result = data[i] * cand_result;
+                std::cout << "answer: " << result << std::endl;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+size_t find_product_for(const std::vector<size_t>& data, size_t num_cands, size_t sum_target)
+{
+    return find_product_for_recurs(data, 0, num_cands, sum_target);
+}
+
 int main()
 {
     std::ifstream inf(PROJEUL_AOC_PATH "/01_input.txt");
@@ -22,5 +60,9 @@ int main()
             std::cout << "answer: " << *it * cand << std::endl;
         }
     }
+    size_t result = find_product_for(data, 2, 2020);
+    std::cout << "result: " << result << std::endl;
+    result = find_product_for(data, 3, 2020);
+    std::cout << "result: " << result << std::endl;
     return 0;
 }
