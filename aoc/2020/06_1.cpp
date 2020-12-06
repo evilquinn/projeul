fc658fe41873e3cc79b932293401f9ea840fd5c7
evilquinn <evilquinn@gmail.com>
2020-12-06 22:27:48 +0000

    aoc 2020 day 6 part 2 done

--- aoc/2020/06_1.cpp
+++ aoc/2020/06_1.cpp
@@ -4,7 +4,10 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <set>
+#include <vector>
+#include <string>
 #include <algorithm>
 
 #include <aoc/path_def.hpp>
@@ -21,12 +24,31 @@ int count_group(std::istream& is)
     return questions.size();
 }
 
+int count_group_everyone(std::istream& is)
+{
+    std::string common = "*";
+    for ( std::string line; std::getline(is, line); )
+    {
+        if ( line.empty() ) break;
+        std::sort(line.begin(), line.end());
+        if ( common == "*" )
+        {
+            common = line;
+            continue;
+        }
+        auto erase_from = std::remove_if(common.begin(), common.end(), [&line](char c){ return line.find(c) == line.npos; });
+        common.erase(erase_from, common.end());
+    }
+    return common.size();
+}
+
 int count_groups(std::istream& is)
 {
     int result = 0;
     while ( is )
     {
-        result += count_group(is);
+        auto res = count_group_everyone(is);
+        result += res;
     }
     return result;
 }
@@ -34,6 +56,17 @@ int count_groups(std::istream& is)
 
 int main()
 {
+#if 0
+    std::vector<std::string> data = {
+        "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb"
+    };
+    for ( auto&& datum : data )
+    {
+        std::istringstream iss(datum);
+        auto res = count_groups(iss);
+        std::cout << "res: " << res << std::endl;
+    }
+#endif
     std::ifstream inf(PROJEUL_AOC_PATH "/06_input.txt");
     int result = count_groups(inf);
     std::cout << "result: " << result << std::endl;
