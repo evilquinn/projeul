c1aef5c1a68bf655a43595cf75d2e5204798e63d
evilquinn <evilquinn@gmail.com>
2020-12-15 01:00:24 +0000

    just the input file and bare exe

---
+++ aoc/2020/13_1.cpp
@@ -0,0 +1,28 @@
+/*
+ * https://adventofcode.com/2020/day/13
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <boost/lexical_cast.hpp>
+
+#include <aoc/path_def.hpp>
+
+
+int main()
+{
+#if 0
+    std::vector<std::string> data = {
+    };
+    for ( auto&& datum : data )
+    {
+    }
+#endif
+#if 1
+    std::ifstream inf(PROJEUL_AOC_PATH "/13_input.txt");
+    std::cout << inf.rdbuf() << std::endl;
+#endif
+    return 0;
+}
