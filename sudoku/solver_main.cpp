edb33b721867b478efc2cd152b14e3dee60d785e
evilquinn <evilquinn@gmail.com>
2021-09-14 19:19:57 +0100

    add couple of as yet unsolved grids

--- sudoku/solver_main.cpp
+++ sudoku/solver_main.cpp
@@ -28,7 +28,7 @@ int main()
     evilquinn::register_signal_handler();
 #if 1
     evilquinn::sudoku::grid_reader reader(
-        "/home/evilquinn/git/projeul/sudoku/p096_sudoku.txt" );
+        "/home/evilquinn/git/projeul/sudoku/p096_sudoku_difficult.txt" );
 
     size_t total_remaining = 0;
     size_t magic_result = 0;
@@ -36,8 +36,9 @@ int main()
     {
         evilquinn::sudoku::solver( *grid_from_file ).solve();
         std::cout << *grid_from_file << std::endl;
-        total_remaining += grid_from_file->count_remaining();
-        magic_result += evilquinn::get_magic(*grid_from_file);
+        size_t remaining = grid_from_file->count_remaining();
+        total_remaining += remaining;
+        if ( remaining == 0 ) magic_result += evilquinn::get_magic(*grid_from_file);
     }
     std::cout << total_remaining << std::endl;
     std::cout << "magic: " << magic_result << std::endl;
