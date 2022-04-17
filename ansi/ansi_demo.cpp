fff47417cbc97bd6ee9e9fafd97af50ab37d0768
evilquinn <evilquinn@gmail.com>
2022-04-17 20:52:22 +0100

    more ansi demo

--- ansi/ansi_demo.cpp
+++ ansi/ansi_demo.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <map>
 #include <iomanip>
 #include "escapes.hpp"
 
@@ -8,31 +9,103 @@ int main()
     std::cout << "hello" << std::endl;
 
     std::cout << "Basic 8 colours:" << std::endl;
-    for ( int i = 30; i < 38; ++i )
+    for ( int i = 0; i < 8; ++i )
     {
-        std::cout << ansi_escapes::colour::by_arg(i) << std::setw(4) << i;
+        std::cout << ansi_escapes::style::by_arg(ansi_escapes::style::foreground, i) << std::setw(4) << i;
     }
     std::cout << std::endl;
     std::cout << "Bright colours:" << std::endl;
-    for ( int i = 30; i < 38; ++i )
+    for ( int i = 0; i < 8; ++i )
     {
-        std::cout << ansi_escapes::colour::brighten(ansi_escapes::colour::by_arg(i)) << std::setw(4) << i;
+        std::cout << ansi_escapes::style::brighten(ansi_escapes::style::by_arg(ansi_escapes::style::foreground, i)) << std::setw(4) << i;
     }
     std::cout << std::endl;
-    std::cout << ansi_escapes::colour::reset() << std::endl;
-    
+    std::cout << ansi_escapes::style::reset << std::endl;
+
+    std::cout << "Basic Backgrounds:" << std::endl;
+    for ( int i = 0; i < 8; ++i )
+    {
+        std::cout << ansi_escapes::style::by_arg(ansi_escapes::style::background, i);
+        for ( int j = 0; j < 8; ++j )
+        {
+            std::cout << ansi_escapes::style::by_arg(ansi_escapes::style::foreground, j) << std::setw(4) << i;
+        }
+        std::cout << ansi_escapes::style::reset << std::endl;
+        std::cout << ansi_escapes::style::by_arg(ansi_escapes::style::background, i);
+        for ( int j = 0; j < 8; ++j )
+        {
+            std::cout << ansi_escapes::style::brighten(ansi_escapes::style::by_arg(ansi_escapes::style::foreground, j)) << std::setw(4) << i;
+        }
+        std::cout << ansi_escapes::style::reset << std::endl;
+
+        std::cout << ansi_escapes::style::brighten(ansi_escapes::style::by_arg(ansi_escapes::style::background, i));
+        for ( int j = 0; j < 8; ++j )
+        {
+            std::cout << ansi_escapes::style::by_arg(ansi_escapes::style::foreground, j) << std::setw(4) << i;
+        }
+        std::cout << ansi_escapes::style::reset << std::endl;
+    }
+    std::cout << ansi_escapes::style::reset << std::endl;
+
     std::cout << "256 colours:" << std::endl;
     for ( int i = 0; i < 16; ++i )
     {
         for ( int j = 0; j < 16; ++j )
         {
             int v = i * 16 + j;
-            std::cout << ansi_escapes::colour::by_arg_256(v) << std::setw(4) << v;
+            std::cout << ansi_escapes::style::by_arg_256(ansi_escapes::style::foreground, v) << std::setw(4) << v;
+            if ( j == 7 ) std::cout << ansi_escapes::style::reset << std::endl;
         }
-        std::cout << ansi_escapes::colour::reset() << std::endl;
+        std::cout << ansi_escapes::style::reset << std::endl;
     }
     std::cout << std::endl;
-    std::cout << ansi_escapes::colour::reset() << std::endl;
-    
+    for ( int i = 0; i < 16; ++i )
+    {
+        for ( int j = 0; j < 16; ++j )
+        {
+            int v = i * 16 + j;
+            std::cout << ansi_escapes::style::by_arg_256(ansi_escapes::style::background, v) << std::setw(4) << v;
+            if ( j == 7 ) std::cout << ansi_escapes::style::reset << std::endl;
+        }
+        std::cout << ansi_escapes::style::reset << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << ansi_escapes::style::reset << std::endl;
+
+    std::cout << "Styles:" << std::endl;
+    enum class style_id { bold, underline, invert };
+    std::map<style_id, std::pair<std::string, std::string> > styles =
+    {
+        { style_id::bold, { "bold", ansi_escapes::style::bold } },
+        { style_id::underline, { "underline", ansi_escapes::style::underline } },
+        { style_id::invert, { "invert", ansi_escapes::style::invert } }
+    };
+    std::cout << ansi_escapes::style::bold
+              << "bold"
+              << ansi_escapes::style::reset << std::endl;
+    std::cout << ansi_escapes::style::underline
+              << "underline"
+              << ansi_escapes::style::reset << std::endl;
+    std::cout << ansi_escapes::style::invert
+              << "invert"
+              << ansi_escapes::style::reset << std::endl;
+    std::cout << ansi_escapes::style::bold
+              << ansi_escapes::style::underline
+              << "bold, underline"
+              << ansi_escapes::style::reset << std::endl;
+    std::cout << ansi_escapes::style::bold
+              << ansi_escapes::style::invert
+              << "bold, invert"
+              << ansi_escapes::style::reset << std::endl;
+    std::cout << ansi_escapes::style::underline
+              << ansi_escapes::style::invert
+              << "underline, invert"
+              << ansi_escapes::style::reset << std::endl;
+    std::cout << ansi_escapes::style::bold
+              << ansi_escapes::style::underline
+              << ansi_escapes::style::invert
+              << "bold, underline, invert"
+              << ansi_escapes::style::reset << std::endl;
+
     return 0;
 }
--- ansi/escapes.hpp
+++ ansi/escapes.hpp
@@ -3,44 +3,69 @@
 
 namespace ansi_escapes
 {
-static const std::string escape = "\033[";
-static const char direction_up   = 'A';
-static const char direction_down = 'B';
-static const char direction_left = 'D';
+const std::string escape = "\033[";
+const char direction_up   = 'A';
+const char direction_down = 'B';
+const char direction_left = 'D';
 std::string move_by(char d, int n)
 {
     return escape + boost::lexical_cast<std::string>(n) + d;
 }
-class move
+namespace move
 {
-public:
-static std::string by(char d, int n) { return escape + boost::lexical_cast<std::string>(n) + d; }
-static std::string up(int n) { return move::by(direction_up, n); }
-static std::string down(int n) { return move::by(direction_down, n); }
-static std::string left(int n) { return move::by(direction_left, n); }
+std::string by(char d, int n) { return escape + boost::lexical_cast<std::string>(n) + d; }
+std::string up(int n) { return move::by(direction_up, n); }
+std::string down(int n) { return move::by(direction_down, n); }
+std::string left(int n) { return move::by(direction_left, n); }
 };
-class colour
+namespace style
 {
-public:
-static std::string black()   { return escape + "30m"; }
-static std::string red()     { return escape + "31m"; }
-static std::string green()   { return escape + "32m"; }
-static std::string yellow()  { return escape + "33m"; }
-static std::string blue()    { return escape + "34m"; }
-static std::string magenta() { return escape + "35m"; }
-static std::string cyan()    { return escape + "36m"; }
-static std::string white()   { return escape + "37m"; }
-static std::string reset()   { return escape + "0m";  }
-static std::string by_arg(int n) { return escape + boost::lexical_cast<std::string>(n) + "m"; }
-static std::string brighten(std::string s) { s.insert(s.find_last_of('m'), ";1"); return s; }
-static std::string by_arg_256(int n) { return escape + "38;5;" + boost::lexical_cast<std::string>(n) + "m"; };
+enum ground_id
+{
+    foreground = '3',
+    background = '4'
 };
-static const std::string clear_current_line    = escape + 'K';
-static const std::string clear_cursor_to_end   = escape + "0K";
-static const std::string clear_cursor_to_start = escape + "1K";
-static const std::string clear_entire_line     = escape + "2K";
+const char style_modifier = 'm';
+namespace fg
+{
+const std::string black   = escape + static_cast<char>(foreground) + "0" + style_modifier;
+const std::string red     = escape + static_cast<char>(foreground) + "1" + style_modifier;
+const std::string green   = escape + static_cast<char>(foreground) + "2" + style_modifier;
+const std::string yellow  = escape + static_cast<char>(foreground) + "3" + style_modifier;
+const std::string blue    = escape + static_cast<char>(foreground) + "4" + style_modifier;
+const std::string magenta = escape + static_cast<char>(foreground) + "5" + style_modifier;
+const std::string cyan    = escape + static_cast<char>(foreground) + "6" + style_modifier;
+const std::string white   = escape + static_cast<char>(foreground) + "7" + style_modifier;
+};
+namespace bg
+{
+const std::string black   = escape + static_cast<char>(background) + "0" + style_modifier;
+const std::string red     = escape + static_cast<char>(background) + "1" + style_modifier;
+const std::string green   = escape + static_cast<char>(background) + "2" + style_modifier;
+const std::string yellow  = escape + static_cast<char>(background) + "3" + style_modifier;
+const std::string blue    = escape + static_cast<char>(background) + "4" + style_modifier;
+const std::string magenta = escape + static_cast<char>(background) + "5" + style_modifier;
+const std::string cyan    = escape + static_cast<char>(background) + "6" + style_modifier;
+const std::string white   = escape + static_cast<char>(background) + "7" + style_modifier;
+};
 
-//static const std::string set_x10_mouse = escape +
+const std::string reset = escape + '0' + style_modifier;
+const std::string brighten_modifier = ";1";
+std::string by_arg(enum ground_id ground, int n) { return escape + static_cast<char>(ground) + boost::lexical_cast<std::string>(n) + style_modifier; }
+std::string by_arg_256(enum ground_id ground, int n) { return escape + static_cast<char>(ground) + "8;5;" + boost::lexical_cast<std::string>(n) + style_modifier; };
+std::string brighten(std::string s) { s.insert(s.find_last_of(style_modifier), brighten_modifier); return s; }
+
+const std::string bold      = escape + '1' + style_modifier;
+const std::string underline = escape + '4' + style_modifier;
+const std::string invert    = escape + '7' + style_modifier;
+};
+
+const std::string clear_current_line    = escape + 'K';
+const std::string clear_cursor_to_end   = escape + "0K";
+const std::string clear_cursor_to_start = escape + "1K";
+const std::string clear_entire_line     = escape + "2K";
+
+//const std::string set_x10_mouse = escape +
 
 #define SET_X10_MOUSE 9
 #define SET_VT200_MOUSE 1000
