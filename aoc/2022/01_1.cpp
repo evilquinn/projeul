af41cefbd8e00f86649228619663e2e8d4eff60a
evilquinn <evilquinn@gmail.com>
2022-12-04 12:46:34 +0000

    aoc 2022 day 1 part 2

--- aoc/2022/01_1.cpp
+++ aoc/2022/01_1.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 #include <boost/lexical_cast.hpp>
 
 #include <aoc/path_def.hpp>
@@ -30,6 +31,13 @@ calories_count_type count_calories(std::istream& input)
     return result;
 }
 
+int count_max_n(calories_count_type& calories, int n)
+{
+    if ( int(calories.size()) < n ) throw std::runtime_error("Not enough things to count");
+    std::partial_sort(calories.begin(), calories.begin() + n, calories.end(), std::greater());
+    return calories[0] + calories[1] + calories[2];
+}
+
 int main()
 {
     std::ifstream input(PROJEUL_AOC_PATH "/01_input.txt");
@@ -39,6 +47,9 @@ int main()
     auto max_pos = std::max_element(calories.begin(), calories.end());
     if (max_pos == calories.end()) throw std::runtime_error("Failed to find max");
     auto max = *max_pos;
-    std::cout << "result: " << max << std::endl;
+    std::cout << "Part 1 result: " << max << std::endl;
+
+    auto max_3_count = count_max_n(calories, 3);
+    std::cout << "Part 2 result: " << max_3_count << std::endl;
     return 0;
 }
