dc50b34137906ec97aa2d496dd969801bc66acfd
evilquinn <evilquinn@gmail.com>
2022-12-11 12:46:18 +0000

    aoc 2022 day 8 part 1

---
+++ aoc/2022/08_1.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <algorithm>
+
+#include <aoc/path_def.hpp>
+#include <aoc/coord.hpp>
+
+struct tree_type
+{
+    int height;
+    bool visible;
+    tree_type() : height(0), visible(false) {}
+    tree_type(int height) : height(height), visible(false) {}
+};
+bool operator< (tree_type const& lhs, tree_type const& rhs)
+{
+    return lhs.height < rhs.height;
+}
+using coord_util::coord;
+using map_type = std::map<coord, tree_type>;
+
+map_type read_map(std::istream& input)
+{
+    map_type result;
+    std::string line;
+    int y = 0;
+    while(std::getline(input, line))
+    {
+        for ( int x = 0; (unsigned)x < line.size(); ++x )
+        {
+            result[coord{x, y}] = tree_type(line[x] - '0');
+        }
+        ++y;
+    }
+
+    return result;
+}
+
+coord max_coord(map_type const& tree_map)
+{
+    return std::max_element(tree_map.begin(), tree_map.end())->first;
+}
+
+size_t count_visible(map_type& tree_map)
+{
+    size_t result = 0;
+    coord boundary = max_coord(tree_map);
+    coord limit = boundary + coord{1, 1};
+    if ( limit.x != limit.y ) throw std::runtime_error("Not a square...");
+    //magic
+    for ( int i = 0; i < limit.x; ++i )
+    {
+        int highest = -1;
+        // do the row from left
+        for ( coord c = coord{ 0, i }; c.x < limit.x; ++c.x )
+        {
+            tree_type& tree = tree_map[c];
+            if ( tree.height > highest )
+            {
+                if ( ! tree.visible ) ++result;
+                tree.visible = true;
+                highest = tree.height;
+            }
+        }
+        highest = -1;
+        // do the row from right
+        for ( coord c = coord{ boundary.x, i }; c.x >= 0; --c.x )
+        {
+            tree_type& tree = tree_map[c];
+            if ( tree.height > highest )
+            {
+                if ( ! tree.visible ) ++result;
+                tree.visible = true;
+                highest = tree.height;
+            }
+        }
+        highest = -1;
+        // do the column from top
+        for ( coord c = coord{ i, 0 }; c.y < limit.y; ++c.y )
+        {
+            tree_type& tree = tree_map[c];
+            if ( tree.height > highest )
+            {
+                if ( ! tree.visible ) ++result;
+                tree.visible = true;
+                highest = tree.height;
+            }
+        }
+        highest = -1;
+        // do the column from bottom
+        for ( coord c = coord{ i, boundary.y }; c.y >= 0; --c.y )
+        {
+            tree_type& tree = tree_map[c];
+            if ( tree.height > highest )
+            {
+                if ( ! tree.visible ) ++result;
+                tree.visible = true;
+                highest = tree.height;
+            }
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    std::ifstream input(PROJEUL_AOC_PATH "/08_input.txt");
+    if ( !input ) throw std::runtime_error("Failed to open input file");
+
+    auto tree_map = read_map(input);
+
+    auto p1 = count_visible(tree_map);
+    std::cout << "Part 1 result: " << p1 << std::endl;
+
+    return 0;
+}
