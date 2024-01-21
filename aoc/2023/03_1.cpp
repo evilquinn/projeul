b8a30fa1b604c88b95cc87c5ef32db5a3fc95e9b
evilquinn <evilquinn@gmail.com>
2024-01-21 23:32:24 +0000

    aoc 2023 day 3 part 1

---
+++ aoc/2023/03_1.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <charconv>
+
+#include <aoc/coord.hpp>
+#include <aoc/path_def.hpp>
+
+using coord = coord_util::basic_coord<int>;
+using coord_incrementer = coord_util::basic_coord_incrementer<coord::value_type>;
+using schematic_t = std::map<coord, char>;
+
+coord max_coord(const schematic_t& schematic)
+{
+    coord result;
+    for ( auto&& c : schematic )
+    {
+        if ( c.first.x > result.x ) result.x = c.first.x;
+        if ( c.first.y > result.y ) result.y = c.first.y;
+    }
+    return result;
+}
+coord limit_coord(const schematic_t& schematic)
+{
+    return max_coord(schematic) + coord{ 1, 1 };
+}
+std::ostream& operator<< (std::ostream& os, const schematic_t& schematic)
+{
+    auto limit = limit_coord(schematic);
+    for(coord::value_type y = 0; y < limit.y; ++y)
+    {
+        for(coord::value_type x = 0; x < limit.x; ++x)
+        {
+            os << schematic.at({ x, y });
+        }
+        os << std::endl;
+    }
+    return os;
+}
+
+schematic_t parse_schematic(std::istream& is)
+{
+    schematic_t result;
+    std::string line;
+    coord::value_type y = 0;
+    while ( std::getline(is, line) )
+    {
+        for( coord::value_type x = 0; x < static_cast<coord::value_type>(line.size()); ++x )
+        {
+            result[{ x, y }] = line[x];
+        }
+        ++y;
+    }
+    return result;
+}
+
+bool is_symbol(unsigned char c)
+{
+    return !std::isdigit(c) && c != '.';
+}
+bool has_adjacent_symbol(const schematic_t& schematic, const coord& c, int size)
+{
+    // check schematic for a symbol at any position around the provided coord/size
+    auto origin = c - coord{ 1, 1 };
+    auto limit = c + coord{ size + 1, 2 }; // trust me, I think
+    auto incrementer = coord_util::make_incrementer(limit, origin);
+    // no need to examine the coords that are part of the number, but meh
+    for ( auto i = origin; coord_util::within_limit(i, limit); incrementer(i) )
+    {
+        if ( ! schematic.contains(i) ) continue;
+        if ( is_symbol(schematic.at(i)) ) return true;
+    }
+    return false;
+}
+size_t something(const schematic_t& schematic)
+{
+    size_t result = 0;
+    auto limit = limit_coord(schematic);
+    auto incrementer = coord_util::make_incrementer(limit);
+    std::string curr_num;
+    coord curr_num_start{ 0, 0 };
+    auto process_gathered_number = [&]()
+    {
+        if ( has_adjacent_symbol(schematic, curr_num_start, static_cast<int>(curr_num.size())) )
+        {
+            size_t num = 0;
+            std::from_chars(curr_num.data(), curr_num.data() + curr_num.size(), num);
+            result += num;
+        }
+        curr_num = "";
+    };
+    for ( coord i{ 0, 0 }; coord_util::within_limit(i, limit); incrementer(i) )
+    {
+        // have we crossed a newline while having gathered a number? process it
+        if ( curr_num.size() != 0 && i.y != curr_num_start.y )
+        {
+            process_gathered_number();
+        }
+        // as you were..
+        auto value = schematic.at(i);
+        if ( std::isdigit(value) )
+        {
+            if ( curr_num.size() == 0 ) curr_num_start = i;
+            curr_num.push_back(value);
+        }
+        else if ( curr_num.size() != 0 )
+        {
+            process_gathered_number();
+        }
+    }
+    return result;
+}
+
+std::string example = "467..114..\n"
+                      "...*......\n"
+                      "..35..633.\n"
+                      "......#...\n"
+                      "617*......\n"
+                      ".....+.58.\n"
+                      "..592.....\n"
+                      "......755.\n"
+                      "...$.*....\n"
+                      ".664.598..\n";
+
+int main()
+{
+
+    //std::istringstream iss(example);
+    std::ifstream ifs(PROJEUL_AOC_PATH "/03_input.txt");
+    auto schema = parse_schematic(ifs);
+    std::cout << "Part 1 result: " << something(schema) << std::endl;
+
+    return 0;
+}
