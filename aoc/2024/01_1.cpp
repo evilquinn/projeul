e190557aa01617fdd1ceffee99f0407791b8322f
evilquinn <evilquinn@gmail.com>
2024-12-07 18:26:53 +0000

    aoc 2024 day 1 part 2 done

--- aoc/2024/01_1.cpp
+++ aoc/2024/01_1.cpp
@@ -1,11 +1,11 @@
 
 #include <algorithm>
-#include <iostream>
-#include <fstream>
-#include <numeric>
-#include <vector>
 #include <aoc/path_def.hpp>
-
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <unordered_set>
+#include <vector>
 
 /**
  * Initial test data
@@ -16,9 +16,15 @@
  * 3   9
  * 3   3
  *
+ * part 1:
  * Sum of the differences between index pairs when sorted should equal 11.
  *
+ * part 2:
+ * Sum of each product of element in lhs multiplied by its count in rhs,
+ * should equal 31.
+ *
  */
+
 typedef std::vector<int> list_type;
 std::ostream& operator<<(std::ostream& os, const list_type& list)
 {
@@ -35,6 +41,24 @@ typedef std::pair<list_type, list_type> list_pair_type;
 std::ostream& operator<<(std::ostream& os, const list_pair_type& lists)
 {
     return os << "{ " << lists.first << ", " << lists.second << " }";
+}
+
+typedef std::unordered_multiset<int> set_type;
+std::ostream& operator<<(std::ostream& os, const set_type& set)
+{
+    os << "{";
+    std::string sep = " ";
+    for (auto&& v : set)
+    {
+        os << sep << v;
+        sep = ", ";
+    }
+    return os << " }";
+}
+typedef std::pair<list_type, set_type> list_set_type;
+std::ostream& operator<<(std::ostream& os, const list_set_type& list_set)
+{
+    return os << "{ " << list_set.first << ", " << list_set.second << " }";
 }
 
 list_pair_type test_lists = { { 3, 4, 2, 1, 3, 3 }, { 4, 3, 5, 3, 9, 3 } };
@@ -66,24 +90,60 @@ list_pair_type read_lists(std::istream& input)
     {
         int lhs, rhs;
         auto scan_result = std::sscanf(line.c_str(), "%d %d", &lhs, &rhs);
-        if ( scan_result == 2 )
+        if (scan_result == 2)
         {
             result.first.push_back(lhs);
             result.second.push_back(rhs);
         }
-        else if (scan_result != EOF) throw std::runtime_error(std::string("Scanf failed on line: ") + line);
+        else if (scan_result != EOF)
+            throw std::runtime_error(std::string("Scanf failed on line: ") +
+                                     line);
     }
     return result;
+}
+
+std::string test_input =
+    "3   4\n"
+    "4   3\n"
+    "2   5\n"
+    "1   3\n"
+    "3   9\n"
+    "3   3\n";
+
+list_set_type parse_lists(const list_pair_type& lists)
+{
+    list_set_type result = {
+        lists.first, set_type{ lists.second.begin(), lists.second.end() }
+    };
+    return result;
+}
+
+int sum_products_element_by_count(const list_set_type& list_set)
+{
+    auto sum_differences = std::transform_reduce(
+        list_set.first.begin(),
+        list_set.first.end(),
+        0L,
+        std::plus{},
+        [&list_set](const int& v) { return v * list_set.second.count(v); });
+    return sum_differences;
 }
 
 int main()
 {
     std::string input_path(PROJEUL_AOC_PATH "/01_input.txt");
     std::ifstream input(input_path);
-    if (!input ) throw std::runtime_error(std::string("Error reading input file: ").append(input_path));
+    if (!input)
+        throw std::runtime_error(
+            std::string("Error reading input file: ").append(input_path));
     auto lists = read_lists(input);
 
     auto result = sort_then_sum_differences(lists);
     std::cout << "Part 1 result: " << result << std::endl;
+
+    auto list_counts = parse_lists(lists);
+    auto result2 = sum_products_element_by_count(list_counts);
+    std::cout << "Part 2 result: " << result2 << std::endl;
+
     return 0;
 }
