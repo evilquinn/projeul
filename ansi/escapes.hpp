55f7297dabf9a1467036ce3b2692b54de1cf4df6
evilquinn <evilquinn@gmail.com>
2025-02-17 21:38:46 +0000

    make ansi_escapes header-only, use std::format rather than boost::lexical_cast to generate strings containing numbers

--- ansi/escapes.hpp
+++ ansi/escapes.hpp
@@ -1,66 +1,74 @@
 #ifndef PROJEUL_ANSI_ESCAPES_ESCAPES_HPP
 #define PROJEUL_ANSI_ESCAPES_ESCAPES_HPP
 
+#include <format>
 #include <string>
-#include <boost/lexical_cast.hpp>
 
 namespace ansi_escapes
 {
-std::string move_by(char d, int n);
+static const std::string escape = "\033[";
+// deprecated, use move::by instead
+inline std::string move_by(char direction, int n) { return std::format("{}{}{}", escape, n, direction); }
 namespace move
 {
-std::string by(char d, int n);
-std::string up(int n);
-std::string down(int n);
-std::string left(int n);
-};
+namespace dir
+{
+static const char up    = 'A';
+static const char down  = 'B';
+static const char right = 'C';
+static const char left  = 'D';
+}  // namespace dir
+inline std::string by(char direction, int n) { return std::format("{}{}{}", escape, n, direction); }
+inline std::string up(int n) { return by(dir::up, n); }
+inline std::string down(int n) { return by(dir::down, n); }
+inline std::string right(int n) { return by(dir::right, n); }
+inline std::string left(int n) { return by(dir::left, n); }
+};  // namespace move
 namespace style
 {
-enum ground_id
-{
-    foreground = '3',
-    background = '4'
-};
-const char style_modifier = 'm';
+static const char foreground     = '3';
+static const char background     = '4';
+static const char style_modifier = 'm';
 namespace fg
 {
-extern const std::string black;
-extern const std::string red;
-extern const std::string green;
-extern const std::string yellow;
-extern const std::string blue;
-extern const std::string magenta;
-extern const std::string cyan;
-extern const std::string white;
-};
+static const std::string black   = std::format("{}{}{}{}", escape, foreground, '0', style_modifier);
+static const std::string red     = std::format("{}{}{}{}", escape, foreground, '1', style_modifier);
+static const std::string green   = std::format("{}{}{}{}", escape, foreground, '2', style_modifier);
+static const std::string yellow  = std::format("{}{}{}{}", escape, foreground, '3', style_modifier);
+static const std::string blue    = std::format("{}{}{}{}", escape, foreground, '4', style_modifier);
+static const std::string magenta = std::format("{}{}{}{}", escape, foreground, '5', style_modifier);
+static const std::string cyan    = std::format("{}{}{}{}", escape, foreground, '6', style_modifier);
+static const std::string white   = std::format("{}{}{}{}", escape, foreground, '7', style_modifier);
+};  // namespace fg
 namespace bg
 {
-extern const std::string black;
-extern const std::string red;
-extern const std::string green;
-extern const std::string yellow;
-extern const std::string blue;
-extern const std::string magenta;
-extern const std::string cyan;
-extern const std::string white;
-};
+static const std::string black   = std::format("{}{}{}{}", escape, background, '0', style_modifier);
+static const std::string red     = std::format("{}{}{}{}", escape, background, '1', style_modifier);
+static const std::string green   = std::format("{}{}{}{}", escape, background, '2', style_modifier);
+static const std::string yellow  = std::format("{}{}{}{}", escape, background, '3', style_modifier);
+static const std::string blue    = std::format("{}{}{}{}", escape, background, '4', style_modifier);
+static const std::string magenta = std::format("{}{}{}{}", escape, background, '5', style_modifier);
+static const std::string cyan    = std::format("{}{}{}{}", escape, background, '6', style_modifier);
+static const std::string white   = std::format("{}{}{}{}", escape, background, '7', style_modifier);
+};  // namespace bg
 
-extern const std::string reset;
-std::string by_arg(enum ground_id ground, int n);
-std::string by_arg_256(enum ground_id ground, int n);
-std::string brighten(std::string s);
+static const std::string reset = std::format("{}{}{}", escape, '0', style_modifier);
+static const std::string brighten_modifier = ";1";
+inline std::string by_arg(char ground, int n) { return std::format("{}{}{}{}", escape, ground, n, style_modifier); }
+inline std::string by_arg_256(char ground, int n) { return std::format("{}{}{}{}{}", escape, ground, "8;5;", n, style_modifier); }
+inline std::string brighten(std::string s) { s.insert(s.find_last_of(style_modifier), brighten_modifier); return s; }
 
-extern const std::string bold;
-extern const std::string underline;
-extern const std::string invert;
-};
+static const std::string bold      = std::format("{}{}{}", escape, '1', style_modifier);
+static const std::string underline = std::format("{}{}{}", escape, '4', style_modifier);
+static const std::string invert    = std::format("{}{}{}", escape, '7', style_modifier);
+};  // namespace style
 
-extern const std::string clear_current_line;
-extern const std::string clear_cursor_to_end;
-extern const std::string clear_cursor_to_start;
-extern const std::string clear_entire_line;
+static const std::string clear_current_line    = std::format("{}{}", escape, 'K');
+static const std::string clear_cursor_to_end   = std::format("{}{}", escape, "0K");
+static const std::string clear_cursor_to_start = std::format("{}{}", escape, "1K");
+static const std::string clear_entire_line     = std::format("{}{}", escape, "2K");
 
-//const std::string set_x10_mouse = escape +
+// const std::string set_x10_mouse = escape +
 
 #define SET_X10_MOUSE 9
 #define SET_VT200_MOUSE 1000
@@ -68,8 +76,6 @@ extern const std::string clear_entire_line;
 #define SET_BTN_EVENT_MOUSE 1002
 #define SET_ANY_EVENT_MOUSE 1003
 
+}  // namespace ansi_escapes
 
-
-} // ansi_escapes
-
-#endif // PROJEUL_ANSI_ESCAPES_ESCAPES_HPP
+#endif  // PROJEUL_ANSI_ESCAPES_ESCAPES_HPP
